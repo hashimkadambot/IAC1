ae5e7f97a53dbf88f868c9f0e3941dbb6ada2950
lubnamuhsin <91695083+lubnamuhsin@users.noreply.github.com>
2022-11-22 10:26:57 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,25 +44,34 @@ resource "azurerm_subnet" "subnet1" {
   }
 }
 
-resource "azurerm_app_service_plan" "plan01" {
+resource "azurerm_service_plan" "plan01" {
   name                = var.app_sevice_plan_name
   location            = azurerm_resource_group.rg01.location
   resource_group_name = azurerm_resource_group.rg01.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
-resource "azurerm_app_service" "example" {
+resource "azurerm_linux_web_app" "example" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg01.location
   resource_group_name = azurerm_resource_group.rg01.name
-  app_service_plan_id = azurerm_app_service_plan.plan01.id
+  service_plan_id     = azurerm_service_plan.plan01.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+}
 }
 
 resource "azurerm_app_service_virtual_network_swift_connection" "example" {
-  app_service_id = azurerm_app_service.example.id
+  app_service_id = azurerm_linux_web_app.example.id
   subnet_id      = azurerm_subnet.subnet1.id
+}
+
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.example.id
+  repo_url           = "https://github.com/freelearn2000/IAC1.git"
+  branch             = "master"
+  use_manual_integration = true
+  use_mercurial      = false
 }
